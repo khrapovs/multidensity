7177481f407a1b2d87a563c8ab187552cb4de083
Stanislav Khrapov <khrapovs@gmail.com>
2015-12-14 16:00:06 +0300

    First commit

---
+++ README.rst
@@ -0,0 +1,14 @@
+Multivariate Densities
+======================
+
+Documentation
+-------------
+
+`Documentation <http://multidensity.readthedocs.org/en/latest/>`_.
+
+Installation
+------------
+
+::
+
+	pip install git+git://github.com/khrapovs/multidensity
---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+setup(name='multidensity',
+      version='1.0',
+      description=('Multivariate Densities'),
+      long_description=long_description,
+      author='Stanislav Khrapov',
+      license='MIT',
+      author_email='khrapovs@gmail.com',
+      url='https://github.com/khrapovs/multidensity',
+      py_modules=['multidensity'],
+      packages=find_packages(),
+      keywords=['skew', 'student', 'distribution', 'pdf', 'cdf', 'simulation'],
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Financial and Insurance Industry',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+      ],
+      )
