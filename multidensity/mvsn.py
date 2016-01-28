0cdeeb91b7e20d87b0004c220fae2b8c7d6ebddd
Stanislav Khrapov <khrapovs@gmail.com>
2016-01-28 15:26:53 +0300

    Shift mean of the MSN distribution to zero

--- multidensity/mvsn.py
+++ multidensity/mvsn.py
@@ -114,7 +114,9 @@ class MvSN(MultiDensity):
 
         """
         if self.mu is None:
-            return np.zeros(self.ndim)
+#            return np.zeros(self.ndim)
+            return -2 * (2 / np.pi)**.5 \
+                * self.const_delta() * self.const_omega()
         else:
             return np.array(self.mu)
 
@@ -130,6 +132,47 @@ class MvSN(MultiDensity):
             return np.eye(self.ndim)
         else:
             return np.array(self.sigma)
+
+    def const_omega(self):
+        """Compute a constant.
+
+        Returns
+        -------
+        (ndim, ) array
+
+        """
+        if self.sigma is None:
+            return np.ones(self.ndim)
+        else:
+            return np.diag(self.const_sigma())**.5
+
+    def const_rho(self):
+        """Compute a constant.
+
+        Returns
+        -------
+        (ndim, ndim) array
+
+        """
+        if self.sigma is None:
+            return np.eye(self.ndim)
+        else:
+            omega = self.const_omega()
+            return self.const_sigma() / (omega[:, np.newaxis] * omega)
+
+    def const_delta(self):
+        """Compute a constant.
+
+        Returns
+        -------
+        (ndim, ) array
+
+        """
+        if self.sigma is None:
+            return self.lam / (1 + np.sum(self.lam ** 2))
+        else:
+            norm_lam = scl.solve(self.const_rho(), self.lam)
+            return norm_lam / (1 + np.sum(norm_lam * self.lam))
 
     def pdf(self, data=None):
         """Probability density function (PDF).
@@ -152,9 +195,9 @@ class MvSN(MultiDensity):
         if data is None:
             raise ValueError('No data given!')
         self.data = np.atleast_2d(data)
-        omega = np.diag(self.const_sigma())**.5
         # (T, k) array
-        norm_diff = np.sum((self.data - self.const_mu()) / omega * self.lam, 1)
+        norm_diff = np.sum((self.data - self.const_mu())
+            / self.const_omega() * self.lam, 1)
         return 2 * scs.multivariate_normal.pdf(self.data, mean=self.const_mu(),
                                                cov=self.const_sigma()) \
             * scs.norm.cdf(norm_diff)
@@ -181,19 +224,18 @@ class MvSN(MultiDensity):
             raise ValueError('No data given!')
         self.data = np.atleast_2d(data)
         # (k, ) array
-        omega = np.diag(self.const_sigma())**.5
-        rho = self.const_sigma() / (omega[:, np.newaxis] * omega)
+        omega = self.const_omega()
+        rho = self.const_rho()
         norm_diff = (self.data - self.const_mu()) / omega
-        norm_lam = scl.solve(rho, self.lam)
-        delta = np.atleast_2d(norm_lam / (1 + np.sum(norm_lam * self.lam)))
-        big_rho = np.bmat([[np.ones((1, 1)), delta], [delta.T, rho]])
+        delta = np.atleast_2d(self.const_delta())
+        rho_ext = np.bmat([[np.ones((1, 1)), delta], [delta.T, rho]])
         norm_diff_ext = np.hstack((np.zeros((self.data.shape[0], 1)),
                                    norm_diff))
         low = -10 * np.ones(1 + self.ndim)
         mean = np.zeros(1 + self.ndim)
         # (http://www.nhsilbert.net/source/2014/04/
         # multivariate-normal-cdf-values-in-python/)
-        mvncdf = [2 * scs.mvn.mvnun(low, x, mean, big_rho)[0]
+        mvncdf = [2 * scs.mvn.mvnun(low, x, mean, rho_ext)[0]
             for x in norm_diff_ext]
         if len(mvncdf) == 1:
             return mvncdf[0]
